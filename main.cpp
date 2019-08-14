2c95003a970d4166e33f7dc46e4c95b6dba3afa2
aaronjwoodard <aaronjwoodard@gmail.com>
2019-08-14 02:06:04 -0600

    shift register works. added licenses

--- main.cpp
+++ main.cpp
@@ -3,26 +3,40 @@
 #include <util/setbaud.h>
 #include "src/Utility/Serial/Serial.hpp"
 #include "src/Utility/Pin/Pin.hpp"
-#include "src/Utility/Analog/Analog.hpp"
-#include "src/TLV0832/TLV0832.hpp"
+#include "src/Utility/Shift/MC74HC595A/MC74HC595A.hpp"
 
 int main() {
 
-    Pin::setMode(B5, OUTPUT);
-    Pin::setMode(C5, INPUT);
-    Pin::setMode(C4, INPUT);
-    Serial::init();
-    Analog::init();
+    uint8_t bit0 = 0b10000001;
+    uint8_t bit1 = 0b01000010;
+    uint8_t bit2 = 0b00100100;
+    uint8_t bit3 = 0b00011000;
+    uint8_t bit4 = 0b00100100;
+    uint8_t bit5 = 0b01000010;
+    uint8_t bit6 = 0b10000001;
+    uint8_t bit7 = 0b00000000;
+    
+    uint8_t bits[8] = {bit0, bit1, bit2, bit3, bit4, bit5, bit6, bit7};
+    
+    uint8_t serialIn = B5;
+    uint8_t outputEnable = B4;
+    uint8_t latchClock = B3;
+    uint8_t shiftClock = B2;
+    uint8_t reset = B1;
+    
+    Pin::setMode(serialIn, OUTPUT);
+    Pin::setMode(outputEnable, OUTPUT);
+    Pin::setMode(latchClock, OUTPUT);
+    Pin::setMode(shiftClock, OUTPUT);
+    Pin::setMode(reset, OUTPUT);
+    
+    MC74HC595A mc = MC74HC595A(serialIn, latchClock, shiftClock, reset, outputEnable);
+    uint8_t i = 0;
     while(1) {
-        uint16_t x = Analog::read(0);
-        Serial::print("X: ");
-        Serial::print(x);
-        Serial::putChar('\t');
-        
-        uint16_t y = Analog::read(1);
-        Serial::print("Y: ");
-        Serial::print(y);
-        Serial::putChar('\n');
+        mc.shiftOut(bits[i++]);
+        if (i >= 8) {
+            i = 0;
+        }
         _delay_ms(1000);
     }
     
--- src/Utility/Pin/Pin.cpp
+++ src/Utility/Pin/Pin.cpp
@@ -1,41 +1,68 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * The MIT License
+ *
+ * Copyright 2019 aaron.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 
-/* 
- * File:   Pins.cpp
- * Author: aaron
+/** 
+ * @file:   Pins.cpp
+ * @author: aaron
  * 
  * Created on December 30, 2018, 2:37 PM
  */
 
+
 #include "Pin.hpp"
-#ifdef __AVR_ATmega328P__ 
-uint16_t* Pin::DDRx[3] = {
-    (uint16_t*) &DDRB,
-    (uint16_t*) &DDRC,
-    (uint16_t*) &DDRD
+
+/**
+ * PORTs, DDRs, and PINs are put in arrays for easy access.
+ * By right shifting the pin values above, you can access each of
+ * these registers by right shifting the pin values above
+ */
+uint16_t* Pin::PORTx[3] = {
+    (uint16_t*) & PORTB,
+    (uint16_t*) & PORTC,
+    (uint16_t*) & PORTD
 };
 
-uint16_t* Pin::PORTx[3] = {
-    (uint16_t*) &PORTB,
-    (uint16_t*) &PORTC,
-    (uint16_t*) &PORTD
+uint16_t* Pin::DDRx[3] = {
+    (uint16_t*) & DDRB,
+    (uint16_t*) & DDRC,
+    (uint16_t*) & DDRD
 };
 
 uint16_t* Pin::PINx[3] = {
-    (uint16_t*) &PINB,
-    (uint16_t*) &PINC,
-    (uint16_t*) &PIND
+    (uint16_t*) & PINB,
+    (uint16_t*) & PINC,
+    (uint16_t*) & PIND
 };
-#else // your MCU pin definitions here
-#error Undefined MCU
-#endif
 
+
+/**
+ * Sets the pin to either high (1) or low (0)
+ * @param pin - the pin to set. @see Pin.hpp
+ * @param val - value to set the pin either 1 or 0
+ */
 void Pin::set(uint8_t pin, uint8_t val) {
-    uint16_t *port = PORTx[pin >> 3];
+    uint16_t *port = PORTx[pin >> 3]; 
     if (val == HIGH) {
         *port |= 1 << (pin & 7);
     } else {
@@ -43,6 +70,11 @@ void Pin::set(uint8_t pin, uint8_t val) {
     }
 }
 
+/**
+ * Sets the mode of the pin. Either output (1) or input (0)
+ * @param pin - the pin to set. @see Pin.hpp
+ * @param val - the value to set it, either 1 or 0
+ */
 void Pin::setMode(uint8_t pin, uint8_t val) {
     uint16_t *ddr = DDRx[pin >> 3];
     if (val == OUTPUT) {
@@ -52,6 +84,11 @@ void Pin::setMode(uint8_t pin, uint8_t val) {
     }
 }
 
+/**
+ * reads the state of a pin
+ * @param pin - the pin to read from. @see Pin.hpp
+ * @return - the value of the pin, either high (1) or low (0)
+ */
 uint8_t Pin::read(uint8_t pin) {
     return *(PINx[pin >> 3]) & (1 << (pin & 7));
 }
